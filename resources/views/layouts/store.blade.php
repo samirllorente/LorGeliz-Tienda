23feac8ca32f09c8aeed7e3d53f400f0ee23cf67
Carlos Arteaga <carteagajimenez@gmail.com>
2021-02-16 17:25:24 -0500

    jquery cdn

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -60,7 +60,8 @@
 
 	<!-- Scripts -->
 	
-    <script src="{{ asset('asset/js/jquery-3.2.1.min.js') }}"></script>
+    {{--<script src="{{ asset('asset/js/jquery-3.2.1.min.js') }}"></script>--}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     {{--<script src="{{ asset('asset/js/bootbox.min.js') }}"></script>--}}
 	<script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/all.js') }}" defer></script>
