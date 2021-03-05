9b9e1dc5383ae02eeed82e2c9d677eaa8f3b1de0
Carlos Arteaga <carteagajimenez@gmail.com>
2021-03-05 14:01:06 -0500

    cdn select2

--- resources/views/admin/productos/create.blade.php
+++ resources/views/admin/productos/create.blade.php
@@ -567,7 +567,8 @@
 @section('scripts')
 
 <!-- Select2 -->
-<script src="{{ asset('adminlte/plugins/select2/js/select2.full.min.js') }}"></script>
+{{--<script src="{{ asset('adminlte/plugins/select2/js/select2.full.min.js') }}"></script>--}}
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script src="{{ asset('adminlte/ckeditor/ckeditor.js') }}"></script>
 
